035e8163fdb864eacff731da75e2c3ed47edc28d
Gabriel Meireles <gabriellmeireles@hotmail.com>
2024-11-26 21:37:48 -0300

    Configuram a navbar, logos e imagem para diferentes telas e para os modos Claro e Escuro.

--- web-client/tailwind.config.js
+++ web-client/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'main_desktop': "url('assets/img/main_desktop.png')",
+        'main_mobile': "url('/assets/img/main_phoneV2.png')",
+      }
+    },
   },
   plugins: [],
 };

513d3d1e4d7db0978ac2bba230c21afa1b65e929
Gabriel Meireles <gabriellmeireles@hotmail.com>
2024-11-29 21:22:53 -0300

    alterda a estrutura de pasta; criado o componente navbar; criado o componente footer; feito layout responsivo e claro/escuro.

--- web-client/src/app/app.component.ts
+++ web-client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './component/navbar/navbar.component';
-import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',

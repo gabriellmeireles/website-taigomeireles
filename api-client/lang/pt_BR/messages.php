b8732ac12ea43425380e955f5b505f5c6af08607
Gabriel Meireles <gabriellmeireles@hotmail.com>
2025-05-07 14:23:43 -0300

    Utilizando o lang para traduzir as validacoes; Criado o arquivo messeges para mensagens padrao.

---
+++ api-client/lang/pt_BR/messages.php
@@ -0,0 +1,13 @@
+<?php
+    return [
+        'success' => [
+            'create' => ':value registrado!',
+            'update' => ':value atualizado!',
+            'delete' => ':value deletado!',
+        ],
+        'error' => [
+            'not_found' => 'Registro não encontrado.',
+            'validation' => 'Erro na validação dos dados.',
+        ]
+    ];
+    
